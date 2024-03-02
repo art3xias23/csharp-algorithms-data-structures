b607e107cd4551e2c023c872a2ab632f9249398a
Tino <Konstantin.v.milchev@gmail.com>
2024-03-02 23:01:35 +0000

    Adding implementation of the floyd has cycle algo

---
+++ Algorithms/playground.cs
@@ -0,0 +1,14 @@
+bool hasCycle<T>(Node<T> head){
+	if(head is null) return false;
+
+	var slow = head;
+	var fast = head.Next;
+
+	while (slow != fast){
+		if(fast.Next is null || fast.Next.Next is null) return false;
+
+		slow = slow.Next;
+		fast = fast.Next.Next;
+	}
+	return true;
+}
